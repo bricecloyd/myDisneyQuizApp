e196edd5dbfcf2b7f87335d57803dcc362d91587
BCloyd <brice.cloyd@gmail.com>
2020-02-17 16:24:57 -0800

    initial commit

---
+++ store.js
@@ -0,0 +1,117 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "How are Frozone, Syndrome and Bomb Voyage connected?",
+        options: [
+            "They went to school together",
+            "They are characters from Disney's animated cartoon 'The Incredibles'",
+            "They share an ancient ancestor from Queens",
+            "They always look for the “Bear Necessities"
+            
+        ],
+        answer: "They are characters from Disney's animated cartoon 'The Incredibles'",
+        incrediblesPic: "./quiz app imgs/Bomb-Voyage.png" 
+      },
+      //2
+      {
+        question: "In Disney's Ratatouille, what is the nasty restaurant critic's name, voiced by Peter O' Tool?",
+        options: [
+          "Anton Ego",
+          "Matt Damon", 
+          "Guy Fieri",
+          "Jafar"
+        ],
+        answer: "Anton Ego"
+      },
+      //3
+      {
+        question: "What was the name of the Disney cartoon character who sang the song, 'I've Got No Strings?'",
+        options: [
+          "Jiminy Cricket", 
+          "Pinocchio", 
+          "Stromboli", 
+          "Felix"
+        ],
+        answer: "Pinocchio"
+      },
+      //4
+      {
+        question: "An owl named Archimedes is featured in which Disney film?", 
+        options: [
+            "The Jungle Book",
+            "The Sword and the Stone",
+            "Call of the Wild",
+            "Castaways"
+        ],
+        answer: "The Sword and the Stone"
+      },
+      //5
+      {
+        question: "In Disney's Fun and Fancy free, what does Bongo the bear ride?",
+        options: [
+          "a unicycle", 
+          "a dinosaur", 
+          "vibes of life", 
+          "a tow truck with a goofy wit"
+        ],
+        answer: "a unicycle"
+      },
+      //6
+      {
+        question: "Orville the albatross can be found in which of Disney's cartoon features?",
+        options: [
+          "The Outback", 
+          "The Rescuers", 
+          "Tom and Jerry", 
+          "Toy Story"
+        ],
+        answer: "The Rescuers"
+      },
+      //7
+      {
+        question: "In the Disney cartoon feature Robin Hood, what was the name of the snake?",
+        options: [
+          "Little John", 
+          "Maid Marian", 
+          "Sir Hiss", 
+          "King George"
+        ],
+        answer: "Sir Hiss"
+      },
+      //8
+      {
+        question: "In the Disney film Aladdin, who did the voice for the character of Jasmine?", 
+        options: [
+          "Linda Larkin", 
+          "Robin Williams", 
+          "Justin Bieber", 
+          "Julia Andrews"
+        ],
+        answer: "Linda Larkin"
+      },
+      //9
+      {
+        question: "The Whale's name in Disney's Pinocchio is what?",
+        options: [
+          "Goliath", 
+          "Jonah", 
+          "Monstro", 
+          "Maleficent"
+        ], 
+        answer: "Monstro"
+      },
+      //10
+      {
+        question: "In the movie, The Great Mouse Detective, what kind of creature is Fidget?",
+        options: [
+          "a cat", 
+          "a rat", 
+          "a bat", 
+          "a badger"
+        ],
+        answer: "a bat"
+      },
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
